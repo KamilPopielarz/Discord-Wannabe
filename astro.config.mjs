83be7353dc04a015470c4e14fa5be88de394454e
KamilPopielarzUG <k.popielarz.565@studms.ug.edu.pl>
2025-11-12 18:17:41 +0100

    Tailwind fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,16 @@ import "dotenv/config";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), sitemap(), tailwind()],
+  integrations: [react(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   server: { port: 3000 },
   adapter: node({
     mode: "standalone",
